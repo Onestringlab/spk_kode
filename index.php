3acf40a0022c16477682769a1662967ecdc14807
Onestring Lab <onestringlab@gmail.com>
2022-12-18 18:09:02 +0700

    index.php bs5

--- index.php
+++ index.php
@@ -9,11 +9,31 @@
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-  <title>Hello, world!</title>
+  <title>Sistem Pendukung Keputusan - Onestring Lab</title>
 </head>
 
 <body>
-  <h1>Hello, world!</h1>
+  <div class=container>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">SPK - OSL</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-link" href="/1_WSM_WPM/">WSM WPM</a>
+            <a class="nav-link" href="/2_SMART/">SMART</a>
+            <a class="nav-link" href="/3_SAW/">SAW</a>
+            <a class="nav-link" href="/4_TOPSIS/">TOPSIS</a>
+            <a class="nav-link" href="/5_Profile_Matching/">Profile Matching</a>
+            <a class="nav-link" href="/6_Moora/">MOORA</a>
+            <a class="nav-link" href="/7_Vikor/">VIKOR</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
 
   <!-- Optional JavaScript; choose one of the two! -->
 
